3b0fb7c9a5ff8176960dedb7f025d2512faceb04
muhammad-ahmad08 <ahmadbutt2080@gmail.com>
2026-07-07 09:34:45 +0500

    chore: updated hero image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "URL('public/hero-img.jpeg')",
+          backgroundImage: "URL('/hero-img.jpeg')",
           
         }}
       />
